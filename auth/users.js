80e3a3da0c3b600f37f8f1aa5c0388238f82184e
flutterdemon <flutterdemon25@gmail.com>
2023-07-04 19:40:13 +0300

    jsonwebtoken intro

--- auth/users.js
+++ auth/users.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const { v4: uuidv4 } = require("uuid");
 
 const router = express.Router();
@@ -10,21 +12,44 @@ router.get("/users", (req, res) => {
   res.json(credentials);
 });
 
+//token authentication
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null)
+    return res.sendStatus(401).json({ message: "No token Available" });
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err)
+      return res.sendStatus(403).json({ message: "Token is no longer valid" });
+    req.user = user;
+    next();
+  });
+};
+
 //register a user
 router.post("/register", async (req, res) => {
   try {
     const { username, password } = req.body;
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(password, salt);
-    credentials.push({
+    const newUser = {
       id: uuidv4(),
       username: username,
       password: hashedPassword,
-    });
-    res.status(201).send();
+    };
+    credentials.push(newUser);
+    const accesstoken = jwt.sign(newUser, process.env.ACCESS_TOKEN_SECRET);
+    res.status(201).json({ accesstoken: accesstoken });
   } catch (e) {
     res.status(500).send();
   }
+});
+
+//private url you cant access unless authorized
+router.get("/details", authenticateToken, (req, res) => {
+  res.send("Welcome our Premium user");
+  res.send(req.user);
 });
 
 //login a user
